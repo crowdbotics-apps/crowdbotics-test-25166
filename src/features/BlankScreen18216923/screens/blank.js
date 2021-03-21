e50c994dc2e5d8d5acaff7520bc2bf03b1cce510
Crowdbotics <team@crowdbotics.com>
2021-03-21 17:40:50 +0000

    Update components

--- src/features/BlankScreen18216923/screens/blank.js
+++ src/features/BlankScreen18216923/screens/blank.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { connect } from "react-redux"
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +11,52 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
-export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { TextInput_10: "" }
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View style={styles.View_5}>
+        <View>
+          <TextInput
+            placeholder="Number Input Placeholder"
+            keyboardType="numeric"
+            value={this.state.TextInput_10}
+            onChangeText={nextValue =>
+              this.setState({ TextInput_10: nextValue })
+            }
+          />
+        </View>
+      </View>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 30,
-  },
-});
+  View_1: {},
+  View_5: { flexDirection: "row" },
+  View_6: {},
+  TextInput_10: {},
+  Button_12: {}
+})
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
